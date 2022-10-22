b07c344f05bc70bba85c52872eedd4c6985b89d3
Esdras <esdras12thiago@gmail.com>
2022-10-22 15:58:05 -0300

    Requisito 7 quase concluido

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -3,58 +3,84 @@ import MyContext from '../context/MyContext';
 
 function Filters() {
   const contexto = useContext(MyContext);
-  function deletColumn() {
-    // target.value.remove();
-  }
   const { name,
     value,
     handleName,
     handleColumn,
     handleComparison,
     handleValue,
+    columnOptions,
+    delets,
+    clearCallback,
+    columnCallback,
     filterCallback,
   } = contexto;
+  // const column = ['population',
+  //   'orbital_period',
+  //   'diameter',
+  //   'rotation_period',
+  //   'surface_water'];
   return (
-    <form>
-      <input
-        type="text"
-        value={ name }
-        onChange={ handleName }
-        data-testid="name-filter"
-      />
-      <select
-        data-testid="column-filter"
-        onChange={ handleColumn }
-        onClick={ deletColumn }
-      >
-        <option value="population" id="oi">population</option>
-        <option value="orbital_period">orbital_period</option>
-        <option value="diameter">diameter</option>
-        <option value="rotation_period">rotation_period</option>
-        <option value="surface_water">surface_water</option>
-      </select>
-      <select
-        data-testid="comparison-filter"
-        onChange={ handleComparison }
-      >
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-      <input
-        type="number"
-        data-testid="value-filter"
-        onChange={ handleValue }
-        value={ value }
-      />
+    <section>
+      <form>
+        <input
+          type="text"
+          value={ name }
+          onChange={ handleName }
+          data-testid="name-filter"
+        />
+        <select
+          data-testid="column-filter"
+          onClick={ handleColumn }
+        >
+          {columnOptions.map((e) => (
+            <option value={ e } key={ e }>{ e }</option>
+          ))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          onChange={ handleComparison }
+        >
+          <option value="maior que">maior que</option>
+          <option value="menor que">menor que</option>
+          <option value="igual a">igual a</option>
+        </select>
+        <input
+          type="number"
+          data-testid="value-filter"
+          onChange={ handleValue }
+          value={ value }
+        />
+        <button
+          type="button"
+          data-testid="button-filter"
+          onClick={ filterCallback }
+        >
+          Filtrar
+        </button>
+      </form>
+      {delets.map((e, i) => (
+        <div key={ i } data-testid="filter">
+          <p>{[`${e.column} ${e.comparison} ${e.value}`]}</p>
+          <button
+            type="button"
+            onClick={ columnCallback }
+            name={ e.column }
+            value={ e.comparison }
+            id={ e.value }
+          >
+            Limpar
+          </button>
+        </div>
+      ))}
       <button
         type="button"
-        data-testid="button-filter"
-        onClick={ filterCallback }
+        data-testid="button-remove-filters"
+        onClick={ clearCallback }
       >
-        Filtrar
+        Remover todas as filtragens
       </button>
-    </form>
+    </section>
   );
 }
 
--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -4,18 +4,44 @@ import MyContext from './MyContext';
 import getPlanets from '../services/PlanetAPI';
 
 function Provider({ children }) {
+  const [originalPlanets, setOriginalPlanets] = useState([]);
   const [planets, setPlanets] = useState([]);
   const [name, setName] = useState('');
   const [column, setColumn] = useState('population');
-  const [columnIndex, setColumnIndex] = useState(0);
+  const [columnOptions, setColumnOptions] = useState([]);
   const [comparison, setComparison] = useState('maior que');
   const [value, setValue] = useState(0);
   const [filter, setFilter] = useState(planets);
+  const [delets, setDelets] = useState([]);
   const [filtered, setFiltered] = useState(false);
 
   const handleColumn = ({ target }) => {
     setColumn(target.value);
-    setColumnIndex(target.selectedIndex);
+  };
+
+  const handleFilters = (event) => {
+    const { target } = event;
+    const { value: comparisonFilter, id: valueFilter, name: columnFilter } = target;
+    const deletedValues = originalPlanets.filter((e) => {
+      switch (comparisonFilter) {
+      case 'maior que': return Number(e[columnFilter]) > Number(valueFilter) ? null : e;
+      case 'menor que': return Number(e[columnFilter]) < Number(valueFilter) ? null : e;
+      default: return Number(e[columnFilter]) === Number(valueFilter) ? null : e;
+      }
+    });
+    const allValues = [...filter, ...deletedValues];
+    // const filtredValues = deletedValues.filter((e) => delets.filter((el) => {
+    //   switch (el.comparison) {
+    //   case 'maior que': return console.log(el.column, el.comparison, el.value);
+    //   case 'menor que': return Number(e[el.column]) < Number(el.value);
+    //   default: return Number(e[el.column]) === Number(el.value);
+    //   }
+    // }));
+    const removeRepeatedValues = allValues.filter((el, i) => allValues.indexOf(el) === i);
+    setPlanets(removeRepeatedValues);
+    setFilter(removeRepeatedValues);
+    setColumnOptions([...columnOptions, target.name]);
+    event.nativeEvent.path[1].remove();
   };
 
   const handleComparison = ({ target }) => {
@@ -32,10 +58,11 @@ function Provider({ children }) {
 
   const planetsResult = async () => {
     const { results } = await getPlanets();
+    setOriginalPlanets(results);
     setPlanets(results);
   };
 
-  const handleButton = (event) => {
+  const handleButton = () => {
     const filtrado = planets.filter((e) => {
       switch (comparison) {
       case 'maior que': return Number(e[column]) > Number(value);
@@ -43,23 +70,53 @@ function Provider({ children }) {
       default: return Number(e[column]) === Number(value);
       }
     });
+    const filtredArray = { column, comparison, value };
+    const columnDeleted = columnOptions.filter((e) => e !== column);
+    setColumnOptions(columnDeleted);
+    setDelets([...delets, filtredArray]);
     setFilter(filtrado);
     setPlanets(filtrado);
     setFiltered(true);
-    console.log(event.nativeEvent.path[1][1]);
-    console.log(column);
-    return event.nativeEvent.path[1][1][columnIndex].remove();
   };
+
+  const columArraySave = () => {
+    const columnArray = ['population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water'];
+    setColumnOptions(columnArray);
+  };
+
+  const clearAll = async () => {
+    setDelets([]);
+    setFiltered(false);
+    setPlanets(originalPlanets);
+    const columnArray = ['population',
+      'orbital_period',
+      'diameter',
+      'rotation_period',
+      'surface_water'];
+    setColumnOptions(columnArray);
+  };
+
+  const clearCallback = useCallback(clearAll, [originalPlanets]);
+
+  const columnCallback = useCallback(handleFilters, [originalPlanets,
+    filter,
+    columnOptions]);
 
   const filterCallback = useCallback(handleButton, [planets,
     comparison,
     value,
     column,
-    columnIndex,
+    columnOptions,
+    delets,
   ]);
 
   useEffect(() => {
     planetsResult();
+    columArraySave();
   }, []);
 
   const contextValue = useMemo(() => ({
@@ -67,15 +124,30 @@ function Provider({ children }) {
     handleColumn,
     handleComparison,
     handleValue,
+    clearCallback,
+    columnCallback,
     filterCallback,
     filtered,
     filter,
+    delets,
     planets,
     name,
     column,
+    columnOptions,
     comparison,
     value,
-  }), [name, planets, column, comparison, value, filter, filtered, filterCallback]);
+  }), [name,
+    planets,
+    column,
+    columnOptions,
+    comparison,
+    value,
+    filter,
+    filtered,
+    delets,
+    clearCallback,
+    columnCallback,
+    filterCallback]);
 
   return (
     <MyContext.Provider value={ contextValue }>
